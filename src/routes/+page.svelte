8b72d6fec22d658b9d19f4cdffc893adf8664b51
cute_catgirl <benpmoon@gmail.com>
2024-11-21 13:26:11 -0700

    Improve layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,26 +11,23 @@
 
 <h1>Mae Moon</h1>
 <br />
-<p>Hi! I'm Mae! I'm a highschool student living in Canada. I'm transfem and use she/her pronouns! I enjoy coding, playing video games, and music.</p>
-<h2 class="section">Projects</h2>
+<p>
+	Hi! I'm Mae! I'm a highschool student living in Canada. I'm transfem and use she/her pronouns! I
+	enjoy coding, playing video games, and music.
+</p>
+<br />
 <ul>
 	<li>
-		<a href="https://github.com/cute-catgirl/Loft">Loft</a>
+		<a href="/experiments"><strong>experiments -></strong></a>
 	</li>
 	<li>
 		<a href="/archive"><strong>archive -></strong></a>
 	</li>
-</ul>
-<h2 class="section">Experiments</h2>
-<ul>
 	<li>
-		<a href="https://cute-catgirl.github.io/the-universe-at-the-end-of-time/">the universe at the end of time</a>
+		<a href="/blog"><strong>blog -></strong></a>
 	</li>
 </ul>
-<h2 class="section">Contact</h2>
-<p>Discord ~ cute_catgirl</p>
-<p>Email ~ maemoon@proton.me</p>
-<h2 class="section">Blog</h2>
+<h2 class="section">Latest posts</h2>
 <ul>
 	{#each data.posts.slice(0, 8) as post}
 		<li transition:fade>
@@ -39,7 +36,9 @@
 			</a>
 		</li>
 	{/each}
-	<a href="/blog"><strong>see more -></strong></a>
 </ul>
+<h2 class="section">Contact</h2>
+<p>Discord ~ cute_catgirl</p>
+<p>Email ~ maemoon@proton.me</p>
 <h2 class="section">Support Me</h2>
 <Supportbutton darkMode={theme.dark}></Supportbutton>
---
+++ src/routes/experiments/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Mae Moon ~ Experiments</h1>
+<br>
+<ul>
+	<li>
+		<a href="https://cute-catgirl.github.io/the-universe-at-the-end-of-time/">the universe at the end of time</a>
+	</li>
+</ul>
